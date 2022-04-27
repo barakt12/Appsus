d5becb0c7ee443c60834bb24e02cb92daf9f54e5
Barak Treves <baraktreves1@gmail.com>
2022-04-27 18:20:33 +0300

    fixed bug

--- apps/mail/cmps/mails-filter.jsx
+++ apps/mail/cmps/mails-filter.jsx
@@ -1,33 +1,4 @@
 export class MailsFilter extends React.Component {
-<<<<<<< HEAD
-    state = {
-        filterBy: {
-            isRead: '',
-            searchInp: '',
-            // isStarred: '',
-            // folder: '', // inbox/sent/drafts/trash            
-        }
-    }
-
-
-
-    handleChange = ({ target }) => {
-        const value = target.value
-        this.setState((prevState) => ({ filterBy: { ...prevState.filterBy, searchInp: value } }),
-            () => this.props.onSetFilter(this.state.filterBy))
-    }
-
-    render() {
-        const { searchInp } = this.state
-
-        return <div className="mail-filter-box-container">
-            <form>
-                <input type="text" placeholder="Search mail" name="searchInp" value={searchInp} onChange={this.handleChange} />
-            </form>
-        </div>
-    }
-}
-=======
   state = {
     filterBy: {
       isRead: '',
@@ -37,8 +8,31 @@ export class MailsFilter extends React.Component {
     },
   }
 
+  handleChange = ({ target }) => {
+    const value = target.value
+    this.setState(
+      (prevState) => ({
+        filterBy: { ...prevState.filterBy, searchInp: value },
+      }),
+      () => this.props.onSetFilter(this.state.filterBy)
+    )
+  }
+
   render() {
-    return <div>mail filter</div>
+    const { searchInp } = this.state
+
+    return (
+      <div className='mail-filter-box-container'>
+        <form>
+          <input
+            type='text'
+            placeholder='Search mail'
+            name='searchInp'
+            value={searchInp}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    )
   }
 }
->>>>>>> 8b5b207b99dd6ffb1c4af6b9535dc741e848585a
